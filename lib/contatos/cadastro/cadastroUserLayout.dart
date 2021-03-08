7ec3dea251e6f0ef7c5e86436e240aeb550a4854
Igo Silva <igosilvari@gmail.com>
2021-03-08 11:39:32 -0300

    Mudança na passagem por campo

--- lib/contatos/cadastro/cadastroUserLayout.dart
+++ lib/contatos/cadastro/cadastroUserLayout.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 import 'package:Agenda_de_Lembretes/contatos/UserCampos/user.dart';
-import 'package:Agenda_de_Lembretes/contatos/iconPerson/iconPerson.dart';
 import 'package:Agenda_de_Lembretes/contatos/lista/listaContatos.dart';
 import 'package:Agenda_de_Lembretes/sql/database.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -64,6 +63,14 @@ class _BodyLayoutState extends State<BodyLayout> {
   }
   //Fim do comando
 
+  final nomeFocus = FocusNode();
+  final emailFocus = FocusNode();
+  final telefoneFocus = FocusNode();
+  final enderecoFocus = FocusNode();
+  final cidadeFocus = FocusNode();
+  final numeroFocus = FocusNode();
+  final ufFocus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,13 +104,13 @@ class _BodyLayoutState extends State<BodyLayout> {
                   await _dbHelper.insertContact(usee);
                   form.reset();
                   await _refreshContactList();
+                  Navigator.of(context).pop();
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return ListContat();
+                    },
+                  ));
                 }
-                Navigator.of(context).pop();
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return ListContat();
-                  },
-                ));
               },
             ),
           ]),
@@ -166,6 +173,13 @@ class _BodyLayoutState extends State<BodyLayout> {
                       }
                       return null;
                     },
+                    autofocus: true,
+                    textInputAction: TextInputAction.next,
+                    focusNode: nomeFocus,
+                    onFieldSubmitted: (term) {
+                      nomeFocus.unfocus();
+                      FocusScope.of(context).requestFocus(telefoneFocus);
+                    },
                     decoration: InputDecoration(
                       labelText: "Nome:",
                       contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
@@ -195,6 +209,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                       }
                       return null;
                     },
+                    textInputAction: TextInputAction.next,
+                    focusNode: telefoneFocus,
+                    onFieldSubmitted: (term) {
+                      telefoneFocus.unfocus();
+                      FocusScope.of(context).requestFocus(emailFocus);
+                    },
                     decoration: InputDecoration(
                         labelText: "Telefone:",
                         contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
@@ -214,6 +234,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                     style: TextStyle(fontWeight: FontWeight.bold),
                     keyboardType: TextInputType.emailAddress,
                     onSaved: (value) => setState(() => usee.email = value),
+                    textInputAction: TextInputAction.next,
+                    focusNode: emailFocus,
+                    onFieldSubmitted: (term) {
+                      emailFocus.unfocus();
+                      FocusScope.of(context).requestFocus(cidadeFocus);
+                    },
                     decoration: InputDecoration(
                       labelText: "E-mail:",
                       contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
@@ -236,6 +262,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                           style: TextStyle(fontWeight: FontWeight.bold),
                           keyboardType: TextInputType.text,
                           onSaved: (value) => setState(() => usee.city = value),
+                          textInputAction: TextInputAction.next,
+                          focusNode: cidadeFocus,
+                          onFieldSubmitted: (term) {
+                            cidadeFocus.unfocus();
+                            FocusScope.of(context).requestFocus(ufFocus);
+                          },
                           decoration: InputDecoration(
                             labelText: "Cidade:",
                             contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
@@ -255,6 +287,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                         child: TextFormField(
                           style: TextStyle(fontWeight: FontWeight.bold),
                           onSaved: (value) => setState(() => usee.uf = value),
+                          textInputAction: TextInputAction.next,
+                          focusNode: ufFocus,
+                          onFieldSubmitted: (term) {
+                            ufFocus.unfocus();
+                            FocusScope.of(context).requestFocus(enderecoFocus);
+                          },
                           validator: (String arg) {
                             if (arg.length >= 3) {
                               return 'Ex: Ce';
@@ -288,6 +326,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                           style: TextStyle(fontWeight: FontWeight.bold),
                           onSaved: (value) =>
                               setState(() => usee.address = value),
+                          textInputAction: TextInputAction.next,
+                          focusNode: enderecoFocus,
+                          onFieldSubmitted: (term) {
+                            enderecoFocus.unfocus();
+                            FocusScope.of(context).requestFocus(numeroFocus);
+                          },
                           decoration: InputDecoration(
                             labelText: "Endereço:",
                             contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
@@ -309,6 +353,12 @@ class _BodyLayoutState extends State<BodyLayout> {
                           keyboardType: TextInputType.streetAddress,
                           onSaved: (String value) =>
                               setState(() => usee.number = value),
+                          focusNode: numeroFocus,
+                          textInputAction: TextInputAction.done,
+                          onFieldSubmitted: (term) {
+                            numeroFocus.unfocus();
+                            FocusScope.of(context).requestFocus();
+                          },
                           decoration: InputDecoration(
                             labelText: "N°:",
                             contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
